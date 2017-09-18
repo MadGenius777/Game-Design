440b702a86b8c0d8da50ead3b452947d2519dda6
wrnathan <wrnathan@ncsu.edu>
2017-09-17 23:30:11 -0400

    added functionality to Inventory.java

--- GamePractice/src/user/Inventory.java
+++ GamePractice/src/user/Inventory.java
@@ -5,6 +5,7 @@ package user;
 
 import java.util.ArrayList;
 
+import objects.Equipment;
 import objects.Item;
 
 /**
@@ -16,14 +17,35 @@ public class Inventory {
     
     public ArrayList<Item> inventory;
     
+    public Equipment[] equipment;
+    
     public Inventory() {      
         inventory = new ArrayList<Item>();
+        equipment = new Equipment[20];
+        
     }
     
     public void resetInventory() {
         inventory = new ArrayList<Item>();
     }
     
+    public void resetEquipment() {
+        equipment = new Equipment[20];
+    }
     
+    public void storeAllEquipment() {
+        for(Equipment a : equipment) {
+            inventory.add(a);            
+        }
+        this.resetEquipment();
+    }
+    
+    public boolean equipItem(int invIndex) {
+        if(inventory.get(invIndex) == null)
+            return false;
+        Equipment equip = (Equipment)inventory.get(invIndex);
+        equipment[equip.getEquipSlot()] = equip;
+        return true;
+    }
     
 }
